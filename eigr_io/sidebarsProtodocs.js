e5197fd9d28274630f4e6498206ce52514461388
Marcel Lanz <marcellanz@n-1.ch>
2022-09-06 23:34:27 +0200

    [docusaurus] huh2 :)

--- eigr_io/sidebarsProtodocs.js
+++ eigr_io/sidebarsProtodocs.js
@@ -1,1 +1,21 @@
-module.exports = {"protodocs":[{"type":"category","label":"Spawn","items":[{"type":"category","label":"/eigr/functions/protocol/actors","items":[{"type":"doc","id":"eigr/functions/protocol/actors/actor.proto"},{"type":"doc","id":"eigr/functions/protocol/actors/protocol.proto"}]},{"type":"category","label":"/google/protobuf","items":[{"type":"doc","id":"google/protobuf/any.proto"}]}]}]};
+module.exports = {
+    "protodocs": [{
+        "type": "category",
+        "label": "Spawn",
+        "items": [{
+            "type": "category",
+            "label": "/eigr/functions/protocol/actors",
+            "items": [{
+                "type": "doc",
+                "id": "eigr/functions/protocol/actors/actor.proto"
+            }, {
+                "type": "doc",
+                "id": "eigr/functions/protocol/actors/protocol.proto"
+            }]
+        }, {
+            "type": "category",
+            "label": "/google/protobuf",
+            "items": [{"type": "doc", "id": "google/protobuf/any.proto"}]
+        }]
+    }]
+};
